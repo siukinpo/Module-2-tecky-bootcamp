88df8ddb6b283c094b78e1f51159fb902082510a
James Siu <siukinpo@gmail.com>
2022-08-30 00:18:19 +0800

    after prereview WSP005

---
+++ WSP005/prelecture.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", function (req: Request, res: Response) {
+  res.end("Hello World");
+});
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/`);
+});
